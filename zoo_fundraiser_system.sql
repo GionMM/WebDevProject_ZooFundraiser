1ef778af0c9c2a3addad43f46bc9f09735e91715
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-16 01:42:55 +0800

    Create zoo_fundraiser_system.sql

---
+++ zoo_fundraiser_system.sql
@@ -0,0 +1,299 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2020 at 06:00 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zoo_fundraiser_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adoption`
+--
+
+CREATE TABLE `adoption` (
+  `adoption_id` int(5) NOT NULL,
+  `user_id` int(5) NOT NULL,
+  `animal_id` int(5) NOT NULL,
+  `amount` float NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `animal`
+--
+
+CREATE TABLE `animal` (
+  `animal_id` int(5) NOT NULL,
+  `animal_name` varchar(20) NOT NULL,
+  `animal_species` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(5) NOT NULL,
+  `user_id` int(5) NOT NULL,
+  `total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation`
+--
+
+CREATE TABLE `donation` (
+  `donation_id` int(5) NOT NULL,
+  `user_id` int(5) NOT NULL,
+  `donation_type` tinyint(1) NOT NULL COMMENT '0 is one-time donation, 1 is annual donation',
+  `amount` float NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merch`
+--
+
+CREATE TABLE `merch` (
+  `merch_id` int(5) NOT NULL,
+  `merch_name` varchar(40) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merch_cart`
+--
+
+CREATE TABLE `merch_cart` (
+  `cart_id` int(5) NOT NULL,
+  `merch_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(5) NOT NULL,
+  `user_id` int(5) NOT NULL,
+  `payment_method` tinyint(1) NOT NULL COMMENT '0 is for delivering perks for premium members, 1 is for Cash on Delivery, 2 is Online Payment',
+  `amount` float NOT NULL,
+  `datetime` datetime NOT NULL,
+  `delivery_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_merch`
+--
+
+CREATE TABLE `orders_merch` (
+  `order_id` int(5) NOT NULL,
+  `merch_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(5) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `fullname` varchar(40) NOT NULL,
+  `user_class` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 is for normal user, 1 is for staff/admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adoption`
+--
+ALTER TABLE `adoption`
+  ADD PRIMARY KEY (`adoption_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `animal_id` (`animal_id`);
+
+--
+-- Indexes for table `animal`
+--
+ALTER TABLE `animal`
+  ADD PRIMARY KEY (`animal_id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`) USING BTREE,
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `donation`
+--
+ALTER TABLE `donation`
+  ADD PRIMARY KEY (`donation_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `merch`
+--
+ALTER TABLE `merch`
+  ADD PRIMARY KEY (`merch_id`);
+
+--
+-- Indexes for table `merch_cart`
+--
+ALTER TABLE `merch_cart`
+  ADD PRIMARY KEY (`cart_id`,`merch_id`),
+  ADD KEY `merch_id` (`merch_id`),
+  ADD KEY `cart_id` (`cart_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `orders_merch`
+--
+ALTER TABLE `orders_merch`
+  ADD PRIMARY KEY (`order_id`,`merch_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `merch_id` (`merch_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adoption`
+--
+ALTER TABLE `adoption`
+  MODIFY `adoption_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `animal`
+--
+ALTER TABLE `animal`
+  MODIFY `animal_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donation`
+--
+ALTER TABLE `donation`
+  MODIFY `donation_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `merch`
+--
+ALTER TABLE `merch`
+  MODIFY `merch_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `adoption`
+--
+ALTER TABLE `adoption`
+  ADD CONSTRAINT `adoption_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `adoption_ibfk_2` FOREIGN KEY (`animal_id`) REFERENCES `animal` (`animal_id`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `donation`
+--
+ALTER TABLE `donation`
+  ADD CONSTRAINT `donation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `merch_cart`
+--
+ALTER TABLE `merch_cart`
+  ADD CONSTRAINT `merch_cart_ibfk_1` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`),
+  ADD CONSTRAINT `merch_cart_ibfk_2` FOREIGN KEY (`merch_id`) REFERENCES `merch` (`merch_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `orders_merch`
+--
+ALTER TABLE `orders_merch`
+  ADD CONSTRAINT `orders_merch_ibfk_1` FOREIGN KEY (`merch_id`) REFERENCES `merch` (`merch_id`),
+  ADD CONSTRAINT `orders_merch_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
