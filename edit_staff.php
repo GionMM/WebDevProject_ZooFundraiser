865491c66825d65ce5f5452ca6886fa366bbd778
GionMM <cocogion1@gmail.com>
2021-01-12 06:52:52 +0800

    Update edit_staff.php

--- edit_staff.php
+++ edit_staff.php
@@ -1,10 +1,28 @@
+<?php 
+session_start();
+//include ('connect.php');
+require_once "config.php";
+//$user= $_SESSION['user_id'];
+
+?>
 <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Update Admin
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
 
 <body>
     <ul class="navigation">
       <li><a href="adminhome.php">Home</a></li>
       <li><a href="adminedit.php">Edit Admin</a></li>
-      <li><a href="admindonate.php">Edit Donation</a></li>
+      <li><a href="admindonate.php">View Donation</a></li>
       <li><a href="adminstore.php">Edit Store</a></li>
       <li><a href="adminadopt.php">Edit Animal Adoption</a></li>
       <li><a href="adminreport.php">Report</a></li>
@@ -12,13 +30,13 @@
 	<hr>
 
 <?php 
- 	include 'connect.php';
+ 	//include 'connect.php';
  	//include 'session.php';
  	//$user_id = $_SESSION['user_id'];
  	$user_id = $_REQUEST['user_id'];
  	$sql = "SELECT * FROM user WHERE user_id ='$user_id'";
 
- 	$result = mysqli_query($connect,$sql);
+ 	$result = mysqli_query($link,$sql);
  	$rows = mysqli_fetch_array($result);
 
 ?>
@@ -62,6 +80,7 @@
 					</td>
 				</tr>
 			</table>
+		</form>
 		</form>
 		</body>
 </html>
