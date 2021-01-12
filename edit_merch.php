9d384f5ee820b2409bda14d34950aacaf14eddfc
GionMM <cocogion1@gmail.com>
2021-01-12 10:12:11 +0800

    Update edit_merch.php

--- edit_merch.php
+++ edit_merch.php
@@ -1,16 +1,35 @@
-<!DOCTYPE html>
+<?php 
+session_start();
+//include ('connect.php');
+require_once "config.php";
+//$user= $_SESSION['user_id'];
 
-<style>
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Update Merchandise
+    </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
+<!--<style>
 body {
     background-color: bisque;
     }
-    </style>
+   </style>-->
 
 <body>
     <ul class="navigation">
       <li><a href="adminhome.php">Home</a></li>
       <li><a href="adminedit.php">Edit Admin</a></li>
-      <li><a href="admindonate.php">Edit Donation</a></li>
+      <li><a href="admindonate.php">View Donation</a></li>
       <li><a href="adminstore.php">Edit Store</a></li>
       <li><a href="adminadopt.php">Edit Animal Adoption</a></li>
       <li><a href="adminreport.php">Report</a></li>
@@ -18,19 +37,18 @@ body {
 	<hr>
 
 <?php 
- 	include 'connect.php';
+ 	//include 'connect.php';
  	$merch_id = $_REQUEST['merch_id'];
  	$sql = "SELECT * FROM merch WHERE merch_id ='$merch_id'";
 
- 	$result = mysqli_query($connect,$sql);
+ 	$result = mysqli_query($link,$sql);
  	$rows = mysqli_fetch_array($result);
-
 ?>
 <form action="edit_merch2.php?merch_id=<?php echo $merch_id; ?>" method="get">
 
 <h2>Update Merchandise</h2>
 
-		<form method="post">
+		<form method="post" enctype="multipart/form-data">
 			<table class="table table-bordered">
 	<Tr>
 		<Td colspan="2"><?php echo @$err;?></Td>
@@ -52,7 +70,7 @@ body {
 
 				<tr>
 					<td>Merchandise Photo</td>
-					<Td><input class="form-control" type="file" value="<?php echo $rows['merch_photo'];?>"  name="merch_photo"/></td>
+					<Td><input class="form-control" type="file" value="<?php echo "<img class='card-img-top' src='data:image/jpeg;base64,".base64_encode($rows['merch_photo'])." alt='Card image cap'>";?>"  name="merch_photo"/></td>
 				</tr>
 				
 					</td>
@@ -71,4 +89,4 @@ body {
 		</form>
 		</body>
 </html>
-	
+
