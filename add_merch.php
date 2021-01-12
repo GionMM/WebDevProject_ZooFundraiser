5e90ed005c672aa2f5d62a2def6529014dac591b
GionMM <cocogion1@gmail.com>
2021-01-12 09:52:24 +0800

    Update add_merch.php

--- add_merch.php
+++ add_merch.php
@@ -1,17 +1,35 @@
+<?php 
+session_start();
+//include ('connect.php');
+require_once "config.php";
+//$user= $_SESSION['user_id'];
+
+?>
+
 <!DOCTYPE html>
-
-<style>
-
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Add Merchandise
+    </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
+<!--<style>
 body {
     background-color: bisque;
     }
-    </style>
+    </style>-->
 
 <body>
     <ul class="navigation">
       <li><a href="adminhome.php">Home</a></li>
       <li><a href="adminedit.php">Edit Admin</a></li>
-      <li><a href="admindonate.php">Edit Donation</a></li>
+      <li><a href="admindonate.php">View Donation</a></li>
       <li><a href="adminstore.php">Edit Store</a></li>
       <li><a href="adminadopt.php">Edit Animal Adoption</a></li>
       <li><a href="adminreport.php">Report</a></li>
@@ -20,7 +38,7 @@ body {
 
 <?php
 error_reporting(0);
-include 'connect.php';
+//include 'connect.php';
 extract($_POST);
 	if(isset($save))
 	{	
@@ -28,14 +46,14 @@ extract($_POST);
 //$user_id = $_GET['user_id'];
 $merch_name = $_POST['merch_name'];
 $price = $_POST['price'];
-$merch_photo = $_POST['merch_photo'];
+$merch_photo = addslashes(file_get_contents($_FILES['merch_photo']['tmp_name']));
 	
-	if ($connect->connect_error) {
+	/*if ($connect->connect_error) {
 		die(" Connection failed: " . $connect->connect_error);
-	}	
+	}*/	
 	
 	$sql = "SELECT merch_name FROM merch WHERE merch_name='$merch_name' ";
-	$result = $connect->query($sql);
+	$result = $link->query($sql);
 	$num_rows=mysqli_num_rows($result);
 	$row = $result->fetch_assoc();
 	
@@ -51,7 +69,7 @@ $merch_photo = $_POST['merch_photo'];
 	
 		$sql2 = "INSERT INTO merch (merch_name, merch_price, merch_photo)
 		values ('$merch_name','$merch_price','$merch_photo')";
-		$result2 = mysqli_query($connect, $sql2);
+		$result2 = mysqli_query($link, $sql2);
 		
 		if($result2)
 		{
@@ -72,7 +90,7 @@ $merch_photo = $_POST['merch_photo'];
 
 
 <h1 class="page-header">Register New Merchandise</h1>
-<form method="post">
+<form method="post" enctype="multipart/form-data">
 			<table class="table table-bordered">
 	<Tr>
 		<Td colspan="2"><?php echo @$err;?></Td>
@@ -96,7 +114,7 @@ $merch_photo = $_POST['merch_photo'];
 				</tr>
 	
 				<Td colspan="2" align="center">
-            	<input type="submit" class="btn btn-success" name="save" value="Add New Merchandise">
+            	<input type="submit" class="btn btn-primary" name="save" value="Add New Merchandise">
 				</td>
 				</tr>
 
@@ -105,7 +123,7 @@ $merch_photo = $_POST['merch_photo'];
 
   <?php 
 	$query = "SELECT * FROM merch_photo ORDER BY merch_id DESC";  
-                $result = mysqli_query($connect, $query);  
+                $result = mysqli_query($link, $query);  
                 while($row = mysqli_fetch_array($result))  
                 {  
                      echo '  
