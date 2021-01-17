fdaed8352c4f8d69ff8fa4c74a1c73c9f432cfa5
supcicak0 <50813527+supcicak0@users.noreply.github.com>
2021-01-18 04:16:22 +0800

    Add files via upload

--- web-dev/portfolio/Aainaa/profile.php
+++ web-dev/portfolio/Aainaa/profile.php
@@ -7,9 +7,33 @@
 	<!-- Bootstrap -->
 	<link href="../../css/bootstrap-4.3.1.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- jquery for scroll spy -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script>
+		if ( $( window ).width() > 992 ) {
+			$( window ).scroll( function () {
+				if ( $( this ).scrollTop() > 500 ) {
+					$( '#navbar_top' ).addClass( "fixed-top" );
+
+				} else {
+					$( '#navbar_top' ).removeClass( "fixed-top" );
+
+				}
+			} );
+		}
+	</script>
 	<style>
 		html {
 			scroll-behavior: smooth;
+		}
+		
+		h1,
+		h2,
+		h3,
+		h5 {
+			/*			font-family: "Times New Roman", Times, serif !important;*/
+			font-family: Baskerville, "Palatino Linotype", Palatino, "Century Schoolbook L", "Times New Roman", "serif";
+			font-weight: bold;
 		}
 		
 		hr {
@@ -39,49 +63,167 @@
 		.checked {
 			color: orange;
 		}
-		.navbar{
-			background-color: white;
-		}
-		.nav-link {
-			color: black;
+		
+		.navbar-light .navbar-nav .show> .nav-link,
+		.navbar-light .navbar-nav .active> .nav-link,
+		.navbar-light .navbar-nav .nav-link.show,
+		.navbar-light .navbar-nav .nav-link.active {
+			color: orange;
 		}
 		
+		.navbar-light .navbar-nav .show> .nav-link,
+		.navbar-light .navbar-nav> .nav-link,
+		.navbar-light .navbar-nav .nav-link.show,
+		.navbar-light .navbar-nav .nav-link {
+			color: rgba(0, 0, 0, 0.8);
+		}
+		
+		.headline {
+			/*font-family: Calibri, "Helvetica", san-serif;*/
+			line-height: 1.5em;
+			color: black;
+			/*			font-size: 20px;*/
+		}
+		
+		.headline:after {
+			content: ' ';
+			display: block;
+			border: 2px solid orange;
+			width: 60px;
+		}
+		
+		element.style {}
+		
+		@media (min-width: 768px) .col-md-6 {
+			-ms-flex: 0 0 50%;
+			flex: 0 0 50%;
+			max-width: 50%;
+		}
+		
+		.col-1,
+		.col-2,
+		.col-3,
+		.col-4,
+		.col-5,
+		.col-6,
+		.col-7,
+		.col-8,
+		.col-9,
+		.col-10,
+		.col-11,
+		.col-12,
+		.col,
+		.col-auto,
+		.col-sm-1,
+		.col-sm-2,
+		.col-sm-3,
+		.col-sm-4,
+		.col-sm-5,
+		.col-sm-6,
+		.col-sm-7,
+		.col-sm-8,
+		.col-sm-9,
+		.col-sm-10,
+		.col-sm-11,
+		.col-sm-12,
+		.col-sm,
+		.col-sm-auto,
+		.col-md-1,
+		.col-md-2,
+		.col-md-3,
+		.col-md-4,
+		.col-md-5,
+		.col-md-6,
+		.col-md-7,
+		.col-md-8,
+		.col-md-9,
+		.col-md-10,
+		.col-md-11,
+		.col-md-12,
+		.col-md,
+		.col-md-auto,
+		.col-lg-1,
+		.col-lg-2,
+		.col-lg-3,
+		.col-lg-4,
+		.col-lg-5,
+		.col-lg-6,
+		.col-lg-7,
+		.col-lg-8,
+		.col-lg-9,
+		.col-lg-10,
+		.col-lg-11,
+		.col-lg-12,
+		.col-lg,
+		.col-lg-auto,
+		.col-xl-1,
+		.col-xl-2,
+		.col-xl-3,
+		.col-xl-4,
+		.col-xl-5,
+		.col-xl-6,
+		.col-xl-7,
+		.col-xl-8,
+		.col-xl-9,
+		.col-xl-10,
+		.col-xl-11,
+		.col-xl-12,
+		.col-xl,
+		.col-xl-auto {
+			padding-left: 0;
+		}
+		
+		/* skill bar */
+		.grey-bar {
+			color: #000!important;
+			background-color: #f1f1f1!important
+		}
+		
+		.green-bar {
+			color: #fff!important;
+			background-color: lightsalmon !important
+		}
 
 	</style>
 </head>
 
-<body>
+<body data-spy="scroll">
 
-	<div class="hero-image">
+	<div class="hero-image" id="home">
 		<div class="hero-text">
 			<h1 style="font-size:50px">Hey there!</h1>
-			<p>I'm Aainaa, web designer and software developer from <i class="fa fa-map-marker"></i> Malaysia.</p>
+			<p>I'm Aainaa, a software developer from <i class="fa fa-map-marker"></i> Malaysia.</p>
 			<i class="fa fa-angle-down fa-3x"></i>
 		</div>
 	</div>
 
-	<nav class="shadow navbar navbar-expand-lg sticky-top ">
+	<nav id="navbar_top" class="shadow navbar navbar-expand-lg bg-light navbar-light">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-		<div class="collapse navbar-collapse justify-content-md-center">
+
+		<div id="navbar-2" class="collapse navbar-collapse justify-content-md-center">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link" href="#">Home</a>
+					<a class="nav-link active" href="#home">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">About</a>
+					<a class="nav-link" href="#about">About</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Skill</a>
+					<a class="nav-link" href="#skill">Skill</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Experiences</a>
+					<a class="nav-link" href="#experience">Experiences</a>
 				</li>
 
 			</ul>
 		</div>
 	</nav>
+
+
+
+
 
 	<div class="container" style="margin-top: 2em; margin-bottom: 2em">
 		<div class="card shadow" style="width: 100%;">
@@ -89,193 +231,95 @@
 
 				<section style="margin-top: 4em">
 					<div class="container">
-						<div class="row">
-							<div class="col-md-4">
-								<img class="shadow rounded-circle" alt="259x259" src="avatar.jpg" data-holder-rendered="true">
+						<div class="row" id="about">
+							<div class="col-md-5">
+								<img class="shadow rounded" alt="400x400" src="avatar.jpg" data-holder-rendered="true" style="width: 400px;">
 							</div>
-							<div class="col-md-8">
+							<div class="col-md-7">
 								<div class="row">
-									<h1>BACKGROUND</h1>
+									<h1><span class="headline">Introduction</span></h1>
+									<!--									<h1>Background</h1>-->
 								</div>
-								<hr>
-								<p>I am an adept web developer and software engineer, and have work with many projects. I am proficient with various programming language. My personal interest include: deep learning, data mining, and learn more about today innovation. </p>
-								<p>As a computer science student, I always find modern technology very fascinating. I like it so much that sometimes I wish I was born a couple of hundred years later so I could see and use all the cool stuff that we can only imagine about now!</p>
-
-							</div>
-						</div>
-
-						<br>
-
-						<div class="row">
-							<div class="col-md-3">
-								<h5>BASIC INFORMATION</h5>
-							</div>
-
-							<div class="col-md-9">
-
-							</div>
-						</div>
-
-						<div class="row">
-							<div class="col-md-3">
-								<hr>
-							</div>
-
-							<div class="col-md-9">
-								<hr>
-								<span style="height: 2px;"></span>
+								<!--								<hr>-->
 								<div class="row">
-									<div class="col-md-2">
+									<p>I am an adept software developer, and have work with many projects. I am proficient with various programming language. My personal interest include: deep learning, data mining, and learn more about today innovation. Aside from software developing, I also have done various web designing projects.</p>
+								</div>
+								<div class="row">
+									<h5><span class="headline">Personal Information</span></h5>
+								</div>
+								<div class="row">
+									<div class="col-md-3">
 										<b>Name:</b>
 									</div>
-									<div class="col-md-10">
+									<div class="col-md-9">
 										Aainaa Nabilah binti Rohaizad
 									</div>
 								</div>
 								<div class="row">
-									<div class="col-md-2">
-										<b>Age:</b>
+									<div class="col-md-3">
+										<b>Age</b>
 									</div>
-									<div class="col-md-10">
+									<div class="col-md-9">
 										22
 									</div>
 								</div>
 								<div class="row">
-									<div class="col-md-2">
-										<b>Email:</b>
-									</div>
-									<div class="col-md-10">
-										aainaa.nabila99@gmail.com
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-2">
-										<b>Phone:</b>
-									</div>
-									<div class="col-md-10">
-										+6012-345-6789
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-2">
+									<div class="col-md-3">
 										<b>Address:</b>
 									</div>
-									<div class="col-md-10">
+									<div class="col-md-9">
 										Taman Desa Saujana, Kajang
 									</div>
 								</div>
 								<div class="row">
-									<div class="col-md-2">
+									<div class="col-md-3">
 										<b>Language:</b>
 									</div>
-									<div class="col-md-10">
-										English, Malay
+									<div class="col-md-9">
+										English, malay
 									</div>
 								</div>
+
 							</div>
 						</div>
 
 						<br>
 
-						<div class="row">
-							<div class="col-md-3">
-								<h5>SKILL</h5>
-							</div>
+						<div class="row" id="skill">
+							<h3><span class="headline">Skill</span></h3>
+							<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
 
-							<div class="col-md-9">
-
-							</div>
 						</div>
 
 						<div class="row">
-							<div class="col-md-3">
-								<hr>
-							</div>
-
-							<div class="col-md-9">
-								<hr>
-								<span style="height: 2px;"></span>
-								<div class="row">
-									<div class="col-md-6">
-										<div class="row">
-											<div class="col-md-4">
-												C++:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star"></span>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-md-4">
-												Java:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star"></span>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-md-4">
-												Python:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star"></span>
-											</div>
-										</div>
-
-									</div>
-									<div class="col-md-6">
-										<div class="row">
-											<div class="col-md-4">
-												Bootstrap:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star"></span>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-md-4">
-												HTML:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-md-4">
-												SQL:
-											</div>
-											<div class="col-md-8">
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star checked"></span>
-												<span class="fa fa-star"></span>
-											</div>
-										</div>
-
-									</div>
+							<div class="col-md-6">
+								<span>C++<i style="float:right;">90%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:90%; height: 10px;text-align: right;"></div>
+								</div>
+								<span>Java<i style="float:right;">80%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:80%; height: 10px;text-align: right;"></div>
+								</div>
+								<span>Python<i style="float:right;">70%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:70%; height: 10px;text-align: right;"></div>
 								</div>
 
+							</div>
+							<div class="col-md-6">
+								<span>HTML<i style="float:right;">100%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:100%; height: 10px;text-align: right;"></div>
+								</div>
+								<span>CSS<i style="float:right;">80%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:80%; height: 10px;text-align: right;"></div>
+								</div>
+								<span>PHP<i style="float:right;">90%</i></span>
+								<div class="grey-bar">
+									<div class="w3-container green-bar" style="width:90%; height: 10px;text-align: right;"></div>
+								</div>
 							</div>
 						</div>
 
@@ -311,9 +355,10 @@
 			</div>
 		</div>
 	</div>
-
-
-
+	<!--	script-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 </html>
