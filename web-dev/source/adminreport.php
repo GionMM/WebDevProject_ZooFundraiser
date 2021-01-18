648f21efe69ff37a36f75a7c4bb689c68fcf6e31
yyeong99 <45126154+yyeong99@users.noreply.github.com>
2021-01-19 02:59:55 +0800

    ref button.css

--- web-dev/source/adminreport.php
+++ web-dev/source/adminreport.php
@@ -23,6 +23,7 @@ require_once "config.php";
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="../css/form-validation.css" rel="stylesheet">
   <link href="../css/album.css" rel="stylesheet">
+  <link href="../css/button.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
